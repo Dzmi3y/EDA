311b952d7c93f6997f7491498d173ffa6676f0d7
Dzmitry <dzmi3y@gmail.com>
2024-12-04 15:53:59 +0300

    Pictures service added

---
+++ EDA.Services.Сatalog/Data/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace EDA.Services.Сatalog.Data
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public Decimal Price { get; set; }
+        public int Count { get; set; }
+
+    }
+}
