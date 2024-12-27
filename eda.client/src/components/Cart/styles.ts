8116b5127467bb35b621bebe48be884a7df2c151
Dzmitry <dzmi3y@gmail.com>
2024-12-27 03:05:54 +0300

    Font and libraries added to client

---
+++ eda.client/src/components/Cart/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  @media (min-width: ${(props) => props.theme.breakpoints.md}) {
+  }
+`;
---
+++ eda.client/src/theme.tsx
@@ -0,0 +1,12 @@
+const theme = {
+  breakpoints: {
+    xs: "480px",
+    sm: "576px",
+    md: "768px",
+    lg: "992px",
+    xl: "1366px",
+    xxl: "1600px",
+  },
+};
+
+export default theme;
