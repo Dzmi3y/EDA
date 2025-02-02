153398d14a3d054dca8e1d32353d4ca9a6c1302d
Dzmitry <dzmi3y@gmail.com>
2025-02-02 14:36:49 +0300

    OrderEventHandler added to Order service

---
+++ EDA.Services.Order/EventHandlers/OrderEventHandler.cs
@@ -0,0 +1,90 @@
+﻿using Confluent.Kafka;
+using EDA.Services.Order.Entities;
+using EDA.Shared.Exceptions;
+using EDA.Shared.Kafka.Consumer;
+using EDA.Shared.Kafka.Enums;
+using EDA.Shared.Kafka.Messages.Requests;
+using EDA.Shared.Kafka.Messages.Responses;
+using EDA.Shared.Kafka.Producer;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace EDA.Services.Order.EventHandlers
+{
+
+    public class OrderEventHandler : KafkaConsumerBase
+    {
+        private readonly ILogger<OrderEventHandler> _logger;
+        private readonly IKafkaProducer _producer;
+        private readonly AppDbContext _db;
+
+        public OrderEventHandler(KafkaConsumerBaseConfig config,
+            ILogger<OrderEventHandler> logger, IKafkaProducer producer, AppDbContext db)
+            : base(config, Topics.SignUpRequest, logger)
+        {
+            _logger = logger;
+            _producer = producer;
+            _db = db;
+        }
+
+        protected override async Task HandleAsync(ConsumeResult<string, string> result)
+        {
+            var responseMessage = new ResponseMessage<string>();
+            try
+            {
+                var message = JsonConvert.DeserializeObject<OrderPageRequestMessage>(result.Message.Value);
+
+                if (message == null)
+                {
+                    _logger.LogError("OrderPageRequestMessage is null");
+                    return;
+                }
+
+                Guid.TryParse(message.OrderId, out Guid orderId);
+                Guid.TryParse(message.UserId, out Guid userId);
+                var totalPrice = message.CartItemList.Sum(ci => ci.Count * ci.Price);
+                var cartItem = message.CartItemList.Select(ci => new CartItem
+                {
+                    Id = Guid.NewGuid(),
+                    Count = ci.Count,
+                    ProductId = Guid.Parse(ci.Id),
+                    OrderId = orderId,
+
+                }).ToList();
+
+                var newOrder = new Entities.Order()
+                {
+                    Id = orderId,
+                    UserId = userId,
+                    Price = totalPrice,
+                    Cart = cartItem
+                };
+
+                await _db.Orders.AddAsync(newOrder);
+                await _db.SaveChangesAsync();
+
+
+                _logger.LogInformation($"Order {orderId} created successfully");
+
+                responseMessage.Status = HttpStatusCode.Created;
+                responseMessage.Payload = $"Order {orderId} created successfully";
+            }
+            catch (UserException userEx)
+            {
+                responseMessage.ExceptionMessage = userEx.Message;
+                responseMessage.Status = HttpStatusCode.BadRequest;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                responseMessage.ExceptionMessage = "Internal server error";
+                responseMessage.Status = HttpStatusCode.InternalServerError;
+            }
+            finally
+            {
+                await _producer.SendMessageAsync(Topics.SignUpResponse,
+                    result.Message.Key, responseMessage.ToString());
+            }
+        }
+    }
+}
--- EDA.Services.Order/Program.cs
+++ EDA.Services.Order/Program.cs
@@ -1,10 +1,28 @@
+using Confluent.Kafka;
 using EDA.Services.Order;
+using EDA.Services.Order.EventHandlers;
+using EDA.Shared.Kafka.Consumer;
+using EDA.Shared.Kafka.Producer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.Configure<ProducerConfig>(builder.Configuration.GetSection("ProducerConfig"));
+builder.Services.AddSingleton(resolver =>
+    resolver.GetRequiredService<IOptions<ProducerConfig>>().Value);
+builder.Services.AddSingleton<IKafkaProducer, KafkaProducer>();
+
+builder.Services.Configure<KafkaConsumerBaseConfig>(builder.Configuration.GetSection("ConsumerConfig"));
+builder.Services.AddSingleton(resolver =>
+    resolver.GetRequiredService<IOptions<KafkaConsumerBaseConfig>>().Value);
+
+builder.Services.AddHostedService<OrderEventHandler>();
+
 
 
 var app = builder.Build();
